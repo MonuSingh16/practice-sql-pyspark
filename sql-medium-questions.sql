cf57997f17e23ab3ddf47f75e77b359eb7a313a6
Monu Singh <monu.singh9203@gmail.com>
2023-05-21 10:55:00 +0530

    added medium question file

---
+++ sql-medium-questions.sql
@@ -0,0 +1,6 @@
+/* Which creators published a monetized video 5 mins or longer 
+followed by a non-monetized video that was also 5 mins or longer? 
+Round intermediary calculations to 2 decimals. Output the email of creators sorted the alphabetical order.
+*/
+
+
